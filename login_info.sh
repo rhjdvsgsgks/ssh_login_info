543032fa1d379d268f1af71c37cc6779eae9c3ef
rhjdvsgsgks <26178113+rhjdvsgsgks@users.noreply.github.com>
2019-08-18 12:50:03 -0500

    add more info for an ip

--- login_info.sh
+++ login_info.sh
@@ -6,9 +6,12 @@ login_ips=$(last -ai|head -n -2|awk '{print $NF}'|sed "/0.0.0.0/d;/$local_ip/d"|
 
 login_ips_only=$(echo "$login_ips"|awk '{print $2}')
 
-#login_country=$(
 for i in $login_ips_only ; do
-	echo "$(echo "$login_ips"|grep "$i"|head -n 1) $(whois "$i"|grep -m1 country | awk '{print $2}')" ;
+	login_country=$(whois "$i"|grep -m1 country | awk '{print $2}')
+	if [ "$login_country"  == "CN" ]
+	then
+		echo "$(echo "$login_ips"|grep "$i"|head -n 1) $(curl -s "https://freeapi.ipip.net/111.34.17.246" |sed "s/,/ /g;s/[[:punct:]]//g"|tr -s " ")"
+	else
+	echo "$(echo "$login_ips"|grep "$i"|head -n 1) $login_country"
+fi
 done
-#)
-
